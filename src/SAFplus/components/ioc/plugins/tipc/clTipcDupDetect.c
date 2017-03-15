c5cd67120c7c21c616a05021deced954c3e9b7be
hungta <hungtath99@gmail.com>
2017-03-15 10:48:48 +0700

    Update clTipcDupDetect.c

--- src/SAFplus/components/ioc/plugins/tipc/clTipcDupDetect.c
+++ src/SAFplus/components/ioc/plugins/tipc/clTipcDupDetect.c
@@ -192,10 +192,10 @@ ClRcT clTipcDoesNodeAlreadyExist(void)
         /* Check whether another TIPC node with same tipc address <z.c.n>,
          * as of this node exists 
          */
-    /* To enable simulation mode, we don't check if address was installed for this node
-    because in this mode, no tipc address is added to this node  */
-#if 0
         tipcAddress = clTipcOwnAddrGet();
+    /* To enable simulation mode, we don't check if address was installed for this node
+    because in this mode, no tipc address is added to this node  */        
+#if 0
         if (tipcAddress == 0) // TIPC not installed or no address assigned
         {
             return CL_IOC_RC(CL_ERR_NO_RESOURCE);
